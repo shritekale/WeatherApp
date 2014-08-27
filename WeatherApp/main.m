7569291c03e59b8a94161f1a0f10b42bdbc55c1f
Shri <shri.tekale@gmail.com>
2014-08-27 15:54:26 +0100

    Initial Commit

---
+++ WeatherApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeatherApp
+//
+//  Created by Davendra Patel on 27/08/2014.
+//  Copyright (c) 2014 Shri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSAppDelegate class]));
+    }
+}
